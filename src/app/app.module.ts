2ed32b535a73c2ed3d8bad5005d550dc59ddd0e2
hugo688 <hugo_lemos688@hotmail.com>
2018-05-18 00:05:37 +0000

    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+// Necessário para o Auth0
+import { IonicStorageModule } from '@ionic/storage';
+import { AuthService} from '../services/auth.service';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from './../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HttpClientModule } from '@angular/common/http'
+import { ProdutoService } from './../services/produto';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ProdutoService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
+  ]
+})
+export class AppModule {}
---
+++ src/services/auth.config.ts
@@ -0,0 +1,10 @@
+// Change this filename to auth.config.ts
+
+export const AUTH_CONFIG = {
+  // Needed for Auth0 (capitalization: ID):
+  clientID: 'FRCwAKzRCgNQy0pExIv8UizePKfqK8fe',
+  // Needed for Auth0Cordova (capitalization: Id):
+  clientId: 'FRCwAKzRCgNQy0pExIv8UizePKfqK8fe',
+  domain: 'hugo688.auth0.com', // e.g., you.auth0.com
+  packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
+};
